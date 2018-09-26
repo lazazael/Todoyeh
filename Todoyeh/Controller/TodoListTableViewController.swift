dea9d2f9585c16b60c80477ae38e77f2dc02aa07
laza <laza@lazas-macbook-air.local>
2018-09-26 21:46:10 +0200

    convert to MVC

--- Todoyeh/Controller/TodoListTableViewController.swift
+++ Todoyeh/Controller/TodoListTableViewController.swift
@@ -13,12 +13,25 @@ class TodoListTableViewController: UITableViewController {
     
     let defaults = UserDefaults.standard
     
-    var itemArray = ["Buy eggs", "Take items"]
+    var itemArray = [Item]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String]
+//        if let items = defaults.array(forKey: "ToDoListArray") as? [String]
+//        {
+//            itemArray = items
+//        }
+        let newItem = Item()
+        newItem.title = "Mike thing"
+        itemArray.append(newItem)
+        
+        
+        let newItem2 = Item()
+        newItem2.title = "Bob thing"
+        itemArray.append(newItem2)
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [Item]
         {
             itemArray = items
         }
@@ -39,7 +52,25 @@ class TodoListTableViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        //        if item.done == true
+        //        {
+        //            cell.accessoryType = .checkmark
+        //        }else
+        //        {
+        //            cell.accessoryType = .none
+        //        }
+        
+        // ternary operator ==>
+        // value = condition ? valueIfTrue : valueIfTrue
+        
+        //cell.accessoryType = item.done == true ? .checkmark : .none
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+
         
         return cell
     }
@@ -48,13 +79,25 @@ class TodoListTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        print(itemArray[indexPath.row])
+        //print(itemArray[indexPath.row])
+
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+//        if itemArray[indexPath.row].done == true
+//        {
+//           itemArray[indexPath.row].done = false
+//        }else
+//        {
+//            itemArray[indexPath.row].done = true
+//        }
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-           tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else{
-           tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        self.tableView.reloadData()
+
+        
+//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+//           tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        } else{
+//           tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -69,10 +112,16 @@ class TodoListTableViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen when the user clicks the add item button on our UI alert
             if textField.text != nil {
-            self.itemArray.append(textField.text!)
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "ToDoListArray")
                 
+            self.tableView.reloadData()
+            }else
+            {
             self.tableView.reloadData()
             }
             //print (textField.text)
---
+++ Todoyeh/New Group/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoyeh
+//
+//  Created by laza on 2018. 09. 26..
+//  Copyright © 2018. laza. All rights reserved.
+//
+
+import Foundation
+
+class Item
+{
+    var title:String = ""
+    var done:Bool = false
+}
