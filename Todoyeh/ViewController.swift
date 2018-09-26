94d72def6d52535fea2e9607a2fd98ca5bd9d183
laza <laza@lazas-macbook-air.local>
2018-09-26 18:40:25 +0200

    Initial Commit

---
+++ Todoyeh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoyeh
+//
+//  Created by laza on 2018. 09. 26..
+//  Copyright © 2018. laza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
